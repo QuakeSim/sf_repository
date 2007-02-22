52e20f57a98b0e52563846338608640e0dc1de1e
Marlon Pierce <marpierc@iu.edu>
2007-02-22 20:05:13 +0000

    Initial import of skeleton GeoFEST service.

---
+++ ExecutionServices/GeoFESTService/src/main/java/cgl/webservices/AntServiceException.java
@@ -0,0 +1,9 @@
+package cgl.webservices;
+
+public class AntServiceException extends Exception {
+    public AntServiceException() {}
+    public AntServiceException(String message) {
+	super(message);
+	
+    }
+}
