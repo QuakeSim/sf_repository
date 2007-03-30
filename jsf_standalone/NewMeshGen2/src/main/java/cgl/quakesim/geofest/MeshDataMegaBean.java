0fe8805d67155607e9abd290cc467d899a475b45
Marlon Pierce <marpierc@iu.edu>
2007-03-30 21:02:07 +0000

    Some helpful beans.

---
+++ jsf_standalone/NewMeshGen2/src/main/java/cgl/quakesim/geofest/MeshDataMegaBean.java
@@ -0,0 +1,72 @@
+package cgl.quakesim.geofest;
+
+/**
+ * This is a mesh bean convenient class, holds the web service response's mesh bean and adds
+ * some additional methods.
+ */
+
+public class MeshDataMegaBean {
+	 
+	 MeshRunBean meshRunBean;
+	 String jnlpLayers;
+	 String jnlpFaults;       //Stringifying fault info suitable for the webstart app.
+	 String plotMesh="true";        //This is a legacy field, will always be "true".
+	 String geoFESTBaseUrlForJnlp;
+	 String projectName;
+	 String userName;
+	 String jobUIDStamp;
+	 
+	 public MeshDataMegaBean() {
+	 }
+
+	 public MeshRunBean getMeshRunBean(){
+		  return meshRunBean;
+	 }
+	 public void setMeshRunBean(MeshRunBean meshRunBean) {
+		  this.meshRunBean=meshRunBean;
+	 }
+	 
+	 public void setJnlpLayers(String jnlpLayers) {
+		  this.jnlpLayers=jnlpLayers;
+	 }
+	 
+	 public String getJnlpLayers() {
+		  return jnlpLayers;
+	 }
+
+	 public void setJnlpFaults(String jnlpFaults) {
+		  this.jnlpFaults=jnlpFaults;
+	 }
+	 
+	 public String getJnlpFaults() {
+		  return jnlpFaults;
+	 }
+
+	 public void setProjectName(String projectName) {
+		  System.out.println(projectName);
+		  this.projectName=projectName;
+	 }
+	 public String getProjectName() {
+		  System.out.println(projectName);
+		  return this.projectName;
+	 }
+
+	 public String getPlotMesh() {return this.plotMesh;}
+	 public void setPlotMesh(String plotMesh) {this.plotMesh=plotMesh;}
+
+	 public String getUserName() {return this.userName;}
+	 public void setUserName(String userName){this.userName=userName;}
+
+	 public String getJobUIDStamp() {return this.jobUIDStamp;}
+	 public void setJobUIDStamp(String jobUIDStamp) {this.jobUIDStamp=jobUIDStamp;}
+
+	 public String getGeoFESTBaseUrlForJnlp() {
+		  System.out.println("geofestbaseurlforjnlp in mega: "+geoFESTBaseUrlForJnlp);
+		  return geoFESTBaseUrlForJnlp;
+	 }
+
+	 public void setGeoFESTBaseUrlForJnlp(String geoFESTBaseUrlForJnlp) {
+		  System.out.println("geofestbaseurlforjnlp in mega: "+geoFESTBaseUrlForJnlp);
+		  this.geoFESTBaseUrlForJnlp=geoFESTBaseUrlForJnlp;
+	 }
+}
