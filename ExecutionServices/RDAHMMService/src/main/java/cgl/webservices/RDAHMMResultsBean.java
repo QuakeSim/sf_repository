a87678e3fc5f91e2d8497a8446d6eb7a66a92799
Marlon Pierce <marpierc@iu.edu>
2007-04-03 19:51:33 +0000

    Bean encapsulation of the result urls.

---
+++ ExecutionServices/RDAHMMService/src/main/java/cgl/webservices/RDAHMMResultsBean.java
@@ -0,0 +1,101 @@
+package cgl.webservices;
+
+public class RDAHMMResultsBean {
+	 String inputUrl;
+	 String rangeUrl;
+	 String qUrl;
+	 String piUrl;
+	 String aUrl;
+	 String minvalUrl;
+	 String maxvalUrl;
+	 String lUrl;
+	 String bUrl;
+	 String stdoutUrl;
+	 String inputXPngUrl;
+	 String inputYPngUrl;
+	 String inputZPngUrl;
+
+	 //Setters
+	 public void setInputUrl(String inputUrl) {
+		  this.inputUrl=inputUrl;
+	 }
+	 public void setRangeUrl(String rangeUrl){
+		  this.rangeUrl=rangeUrl;
+	 }
+	 public void setQUrl(String qUrl){
+		  this.qUrl=qUrl;
+	 }
+	 public void setPiUrl(String piUrl) {
+		  this.piUrl=piUrl;
+	 }
+	 public void setAUrl(String aUrl) {
+		  this.aUrl=aUrl;
+	 }
+	 public void setLUrl(String lUrl) {
+		  this.lUrl=lUrl;
+	 }
+	 public void setBUrl(String bUrl) {
+		  this.bUrl=bUrl;
+	 }
+	 public void setMinvalUrl(String minvalUrl) {
+		  this.minvalUrl=minvalUrl;
+	 }
+	 public void setMaxvalUrl(String maxvalUrl) {
+		  this.maxvalUrl=maxvalUrl;
+	 }
+	 public void setStdoutUrl(String stdoutUrl) {
+		  this.stdoutUrl=stdoutUrl;
+	 }
+	 public void setInputXPngUrl(String inputXPngUrl) {
+		  this.inputXPngUrl=inputXPngUrl;
+	 }
+	 public void setInputYPngUrl(String inputYPngUrl) {
+		  this.inputYPngUrl=inputYPngUrl;
+	 }
+	 public void setInputZPngUrl(String inputZPngUrl) {
+		  this.inputZPngUrl=inputZPngUrl;
+	 }
+
+	 //Getters
+	 public String getInputUrl() {
+		  return this.inputUrl;
+	 }
+	 public String getRangeUrl(){
+		  return this.rangeUrl;
+	 }
+	 public String getQUrl(){
+		  return this.qUrl;
+	 }
+	 public String getPiUrl() {
+		  return this.piUrl;
+	 }
+	 public String getAUrl() {
+		  return this.aUrl;
+	 }
+	 public String getLUrl() {
+		  return this.lUrl;
+	 }
+	 public String getBUrl() {
+		  return this.bUrl;
+	 }
+	 public String getMinvalUrl() {
+		  return this.minvalUrl;
+	 }
+	 public String getMaxvalUrl() {
+		  return this.maxvalUrl;
+	 }
+	 public String getStdoutUrl() {
+		  return this.stdoutUrl;
+	 }
+	 public String getInputXPngUrl() {
+		  return this.inputXPngUrl;
+	 }
+	 public String getInputYPngUrl() {
+		  return this.inputYPngUrl;
+	 }
+	 public String getInputZPngUrl() {
+		  return this.inputZPngUrl;
+	 }
+
+
+}
