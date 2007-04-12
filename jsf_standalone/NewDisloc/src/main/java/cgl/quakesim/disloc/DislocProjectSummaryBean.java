dae54204d1772c197419dec9941a5667aa479f73
Marlon Pierce <marpierc@iu.edu>
2007-04-12 02:15:54 +0000

    Added a bean to hold project results and a jsf page to display these results.

---
+++ jsf_standalone/NewDisloc/src/main/java/cgl/quakesim/disloc/DislocProjectSummaryBean.java
@@ -0,0 +1,45 @@
+package cgl.quakesim.disloc;
+
+
+public class DislocProjectSummaryBean {
+	 DislocResultsBean resultsBean;
+	 DislocParamsBean paramsBean;
+	 String userName;
+	 String projectName;
+	 String jobUIDStamp;
+	 String creationDate;
+
+	 public DislocProjectSummaryBean() {
+	 }
+
+	 public String getCreationDate() { return this.creationDate; }
+	 public void setCreationDate(String creationDate) { this.creationDate=creationDate; }
+
+	 public String getUserName(){
+		  return userName;
+	 }
+	 public void setUserName(String userName){
+		  this.userName=userName;
+	 }
+	 
+	 public String getProjectName() {
+		  return projectName;
+	 }
+	 
+	 public void setProjectName(String projectName) {
+		  this.projectName=projectName;
+	 }
+	 
+	 public String getJobUIDStamp() {
+		  return jobUIDStamp;
+	 }
+	 public void setJobUIDStamp(String jobUIDStamp) {
+		  this.jobUIDStamp=jobUIDStamp;
+	 }
+
+	 public DislocResultsBean getResultsBean() { return this.resultsBean; }
+	 public void setResultsBean(DislocResultsBean resultsBean) { this.resultsBean=resultsBean; }
+	 
+	 public DislocParamsBean getParamsBean() { return this.paramsBean; }
+	 public void setParamsBean(DislocParamsBean paramsBean) { this.paramsBean=paramsBean; }
+}
