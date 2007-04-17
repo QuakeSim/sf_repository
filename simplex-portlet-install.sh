f6fb87376c1c3b9926bc59457a212d3e3816532c
Zhigang Qi <qizhigang@users.sourceforge.net>
2007-04-17 20:05:42 +0000

    update for simplex

--- simplex-portlet-install.sh
+++ simplex-portlet-install.sh
@@ -11,11 +11,18 @@ source ./env.sh
 
 echo ""
 echo "######################################"
-echo "Customizing portal"
+echo "Installing SIMPLEX Portlet"
 echo "######################################"
 cd $SIMPLEX_PORTLET_HOME
-mvn clean package
-cp -r target/Simplex $CATALINA_HOME/webapps
+export PORTLET_NAME=Simplex-portlet
+if mvn clean package -Dtomcat.base.dir=$CATALINA_HOME -Dportal.host.name=$PORTAL_HOST_NAME
+then 
+    echo Install complete
+else
+    exit 1
+fi
 
+cp -r target/$PORTLET_NAME $CATALINA_HOME/webapps
+touch $PORTAL_WEBAPP_DIR/WEB-INF/CustomPortal/portlets/$PORTLET_NAME
 
 
