2513da46ed871a1242a5cf24e3fd2798fc1c8dea
Zhigang Qi <qizhigang@users.sourceforge.net>
2007-06-18 19:09:49 +0000

    update for Simplex Excute service

---
+++ ExecutionServices/SimpleXService/src/main/java/cgl/simplex/SimpleXDataKmlService.java
@@ -0,0 +1,16 @@
+/**
+ * SimpleXDataKmlService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.3 Oct 05, 2005 (05:23:37 EDT) WSDL2Java emitter.
+ */
+
+package cgl.webservices.simplex;
+
+public interface SimpleXDataKmlService extends javax.xml.rpc.Service {
+    public java.lang.String getKmlGeneratorAddress();
+
+    public SimpleXDataKml getKmlGenerator() throws javax.xml.rpc.ServiceException;
+
+    public SimpleXDataKml getKmlGenerator(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
