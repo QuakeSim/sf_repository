172ce1749c72f6b6ab1f05fec2cfcc5e8b34899d
Zhigang Qi <qizhigang@users.sourceforge.net>
2007-06-20 19:24:21 +0000

    update for kml generator

--- ExecutionServices/SimpleXService/src/main/java/cgl/simplex/SimpleXDataKml.java
+++ ExecutionServices/SimpleXService/src/main/java/cgl/simplex/SimpleXDataKml.java
@@ -10,7 +10,7 @@ package cgl.webservices.simplex;
 public interface SimpleXDataKml extends java.rmi.Remote {
     public void setDatalist(PointEntry[] inputDataList) throws java.rmi.RemoteException;
     public java.lang.String runMakeKml(java.lang.String serverTag, java.lang.String userName, java.lang.String projectName, java.lang.String jobUID) throws java.rmi.RemoteException;
-    public void setGridLine(java.lang.String folderName, double start_x, double start_y, double end_x, double end_y, double interval) throws java.rmi.RemoteException;
+    public void setGridLine(java.lang.String folderName, double start_x, double start_y, double end_x, double end_y, double xinterval,double yinterval) throws java.rmi.RemoteException;
     public void setPointPlacemark(java.lang.String folderName) throws java.rmi.RemoteException;
     public void setArrowPlacemark(java.lang.String folderName, java.lang.String lineColor, double lineWidth) throws java.rmi.RemoteException;
     public void setCoordinateUnit(java.lang.String unit) throws java.rmi.RemoteException;
