1a70c31de71b48fb592502a08425d24cfc0381a9
Marlon Pierce <marpierc@iu.edu>
2007-07-18 17:18:27 +0000

    Cleanup

--- portlets/Simplex-portlet/src/main/webapp/icon/convert_base_green.sh
+++
@@ -1,9 +0,0 @@
-#####################################################
-COUNTER=0
-while [ $COUNTER -lt 361 ]
-do
-
-COUNTER=`expr $COUNTER + 1`
-convert -background none -rotate $COUNTER base_green.png base_green$COUNTER.png
-
-done
