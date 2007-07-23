dc3e94c1d99bb68b0b53443eeaca543ed259858b
Marlon Pierce <marpierc@iu.edu>
2007-07-23 17:37:40 +0000

    Deleting obsolete directory.

--- jsf_standalone/Simplex/src/main/webapp/icon/convert_base_red.sh
+++
@@ -1,9 +0,0 @@
-#####################################################
-COUNTER=0
-while [ $COUNTER -lt 361 ]
-do
-
-COUNTER=`expr $COUNTER + 1`
-convert -background none -rotate $COUNTER base_red.png base_red$COUNTER.png
-
-done
