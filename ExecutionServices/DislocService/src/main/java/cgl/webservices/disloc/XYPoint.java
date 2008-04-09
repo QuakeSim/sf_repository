dffc9a5b29e9d777078933d99ab49fa73a6f6e56
Marlon Pierce <marpierc@iu.edu>
2008-04-09 21:03:47 +0000

    Need this class to collect the cartesian coordinates associated with a particular observation point.

---
+++ ExecutionServices/DislocService/src/main/java/cgl/webservices/disloc/XYPoint.java
@@ -0,0 +1,22 @@
+package cgl.webservices.disloc;
+
+public class XYPoint {
+	 double x;
+	 double y;
+
+	 public double getX() {
+		  return x;
+	 }
+
+	 public void setX(double x) {
+		  this.x=x;
+	 }
+
+	 public double getY() {
+		  return y;
+	 }
+	 
+	 public void setY(double y) {
+		  this.y=y;
+	 }
+}
