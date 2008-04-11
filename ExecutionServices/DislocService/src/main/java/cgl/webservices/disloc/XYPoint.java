7c1f0be5b4c4f99ea7cc1d3cc64d458d73d0b949
Marlon Pierce <marpierc@iu.edu>
2008-04-11 16:27:35 +0000

    Keep also the lat/lon.

--- ExecutionServices/DislocService/src/main/java/cgl/webservices/disloc/XYPoint.java
+++ ExecutionServices/DislocService/src/main/java/cgl/webservices/disloc/XYPoint.java
@@ -3,6 +3,37 @@ package cgl.webservices.disloc;
 public class XYPoint {
 	 double x;
 	 double y;
+	 private double lon;
+	 
+	 private double lat;
+
+
+    public XYPoint(
+						 double x,
+						 double y,
+						 double lat,
+						 double lon) {
+           this.x = x;
+           this.y = y;
+			  this.lat=lat;
+			  this.lon=lon;
+    }
+
+	 public double getLon() {
+		  return lon;
+	 }
+	 
+	 public double getLat() {
+		  return lat;
+	 }
+
+	 public void setLon(double lon) {
+		  this.lon=lon;
+	 }
+
+	 public void setLat(double lat){
+		  this.lat=lat;
+	 }
 
 	 public double getX() {
 		  return x;
