7542f05264329b4e9b14b243cfb63ab64ea766dd
Xiaoming Gao <nkobie@users.sourceforge.net>
2010-07-10 21:10:54 +0000

    In daily RDAHMM service, some property value patterns are changed in the *.properties files

--- ExecutionServices/DailyRDAHMMService/src/main/resources/DailyRdahmmRunner.sh
+++ ExecutionServices/DailyRDAHMMService/src/main/resources/DailyRdahmmRunner.sh
@@ -6,14 +6,14 @@ PROPFILE=$2
 echo $SUBDIR
 echo $PROPFILE
 
-rm -f -r /globalhome/xgao/projects/crisisgrid/QuakeSim2/portal_deploy/apache-tomcat-5.5.20/webapps/rdahmmexec/daily/$SUBDIR/*-*
-mkdir /globalhome/xgao/projects/crisisgrid/QuakeSim2/portal_deploy/apache-tomcat-5.5.20/webapps/rdahmmexec/daily/$SUBDIR
-rm -f -r /globalhome/xgao/projects/crisisgrid/QuakeSim2/portal_deploy/apache-tomcat-5.5.20/webapps/DailyRDAHMM-portlet/WEB-INF/config/*tmp*
-rm -f -r /globalhome/xgao/RDAHMM_WDIR/$SUBDIR/*-*
+rm -f -r ${tomcat.base.dir}/webapps/${pom.artifactId}/daily/$SUBDIR/*-*
+mkdir ${tomcat.base.dir}/webapps/${pom.artifactId}/daily/$SUBDIR
+rm -f -r ${tomcat.base.dir}/webapps/DailyRDAHMM-portlet/WEB-INF/config/*tmp*
+rm -f -r ${user.home}/DAILY_RDAHMM_WDIR/$SUBDIR/*-*
 
-JARS="/globalhome/xgao/projects/crisisgrid/QuakeSim2/portal_deploy/apache-tomcat-5.5.20/webapps/rdahmmexec/WEB-INF/lib/ant-1.6.5.jar";
+JARS="${tomcat.base.dir}/webapps/${pom.artifactId}/WEB-INF/lib/ant-1.6.5.jar";
 
-for i in /globalhome/xgao/projects/crisisgrid/QuakeSim2/portal_deploy/apache-tomcat-5.5.20/webapps/rdahmmexec/WEB-INF/lib/*.jar;
+for i in ${tomcat.base.dir}/webapps/${pom.artifactId}/WEB-INF/lib/*.jar;
 do
         JARS=$JARS:$i;
 done;
--- ExecutionServices/DailyRDAHMMService/src/main/resources/startDateChecker.sh
+++ ExecutionServices/DailyRDAHMMService/src/main/resources/startDateChecker.sh
@@ -6,9 +6,9 @@ OUTPUTPATH=$2
 echo $WORKDIR
 echo $OUTPUTPATH
 
-JARS="/globalhome/xgao/projects/crisisgrid/QuakeSim2/portal_deploy/apache-tomcat-5.5.20/webapps/rdahmmexec/WEB-INF/lib/ant-1.6.5.jar";
+JARS="${tomcat.base.dir}/webapps/${pom.artifactId}//WEB-INF/lib/ant-1.6.5.jar";
 
-for i in /globalhome/xgao/projects/crisisgrid/QuakeSim2/portal_deploy/apache-tomcat-5.5.20/webapps/rdahmmexec/WEB-INF/lib/*.jar;
+for i in ${tomcat.base.dir}/webapps/${pom.artifactId}/WEB-INF/lib/*.jar;
 do
         JARS=$JARS:$i;
 done;
