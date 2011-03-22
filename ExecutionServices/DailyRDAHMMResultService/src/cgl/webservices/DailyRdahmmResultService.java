7f009d8b6d27a520ac021610710f54c682ed8e28
Xiaoming Gao <nkobie@users.sourceforge.net>
2011-03-22 03:43:26 +0000

    In daily RDAHMM result service, corrected some bugs about plot and KML generating functions.

--- ExecutionServices/DailyRDAHMMResultService/src/cgl/webservices/DailyRdahmmResultService.java
+++ ExecutionServices/DailyRDAHMMResultService/src/cgl/webservices/DailyRdahmmResultService.java
@@ -404,7 +404,198 @@ public class DailyRdahmmResultService {
 			writer.close();
 			fw.close();
 			
-			String res = UtilSet.exec("zip " + kmzPath + " " + kmlPath, new File(destKmlDir));
+			String res = UtilSet.exec("zip -D " + kmzPath + " " + kmlPath, new File(destKmlDir));
+			res = res.toLowerCase();
+			if (res.indexOf("error") >= 0 || res.indexOf("not found") >= 0) {
+				return kmlUrlPattern.replace("<fileName>", kmlFileName);
+			} else {
+				File fileKml = new File(kmlPath);
+				fileKml.delete();
+				return kmlUrlPattern.replace("<fileName>", kmzFileName);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+	
+	/**
+	 * make a kmz file for time between fromDateStr and toDateStr, and return the url of the kml
+	 * @param fromDateStr
+	 * @param toDateStr
+	 * @param resUrl
+	 * @return
+	 */
+	public String getKmlForDateRange2(String fromDateStr, String toDateStr, String resUrl) {
+		deleteObsoleteKml();
+		String contextGroup = "SOPAC";
+		if (resUrl.toUpperCase().indexOf("JPL") >= 0) {
+			contextGroup = "JPL";
+		}
+		String preTreat = "FILL";
+		if (resUrl.toUpperCase().indexOf("NOFILL") >= 0) {
+			preTreat = "NOFILL";
+		}
+		
+		Calendar calFrom = UtilSet.getDateFromString(fromDateStr);
+		Calendar calTo = UtilSet.getDateFromString(toDateStr);
+		if (calFrom.compareTo(calTo) > 0) {
+			Calendar calTmp = calFrom;
+			calFrom = calTo;
+			calTo = calTmp;
+		}
+		Calendar today = Calendar.getInstance();
+		String todayStr = UtilSet.getDateString(today);
+		fromDateStr = UtilSet.getDateString(calFrom);
+		toDateStr = UtilSet.getDateString(calTo);
+		
+		// create the directory for the kmz
+		String kmzDirName = contextGroup + "_" + preTreat + "_" + fromDateStr + "to" + toDateStr + "on" + todayStr;
+		String kmzDirPath = destKmlDir + File.separator + kmzDirName;
+		String kmzFileName = kmzDirName + ".kmz";
+		String kmzFilePath = destKmlDir + File.separator + kmzFileName;
+		File kmzFile = new File(kmzFilePath);
+		if (kmzFile.exists() && kmzFile.isFile()) {
+			return kmlUrlPattern.replace("<fileName>", kmzFileName);
+		}
+		
+		File kmzDirFile = new File(kmzDirPath);
+		if (kmzDirFile.exists()) {
+			if (kmzDirFile.isFile()) {
+				kmzDirFile.delete();
+			} else {
+				UtilSet.deleteDirectory(kmzDirFile);
+			}
+		}
+		kmzDirFile.mkdirs();
+		
+		// create the directory for sub kmls
+		String subDirName = "subdocs";
+		String subDirPath = kmzDirPath + File.separator + subDirName;
+		File subDirFile = new File(subDirPath);
+		subDirFile.mkdir();
+		
+		// create the top level kml
+		String kmlFileName = contextGroup + "_" + preTreat + "_" + fromDateStr + "to" + toDateStr + "on" + todayStr + ".kml";
+		String kmlPath = kmzDirPath + File.separator + kmlFileName;		
+		Document kmlDoc = DocumentHelper.createDocument();
+		Element eleKml = kmlDoc.addElement("kml");
+		eleKml.addAttribute("xmlns", "http://www.opengis.net/kml/2.2");
+		Element eleFolder = eleKml.addElement("Folder");
+		eleFolder.addElement("open").setText("1");		
+		
+		Calendar calTmp = Calendar.getInstance();
+		calTmp.setTimeInMillis(calFrom.getTimeInMillis());
+		Calendar calTmp2 = Calendar.getInstance();
+		calTmp2.set(Calendar.MONTH, Calendar.DECEMBER);
+		calTmp2.set(Calendar.DAY_OF_MONTH, 31);
+		String tmpFromDateStr;
+		String tmpToDateStr;
+		while (calTmp.get(Calendar.YEAR) <= calTo.get(Calendar.YEAR)) {
+			if (calTmp.get(Calendar.YEAR) < calTo.get(Calendar.YEAR)) {
+				calTmp2.set(Calendar.YEAR, calTmp.get(Calendar.YEAR));
+				tmpFromDateStr = UtilSet.getDateString(calTmp);
+				tmpToDateStr = UtilSet.getDateString(calTmp2);
+			} else {
+				tmpFromDateStr = UtilSet.getDateString(calTmp);
+				tmpToDateStr = UtilSet.getDateString(calTo);
+			}
+			String relSubDocPath = subDirName + File.separator + tmpFromDateStr + "to" + tmpToDateStr + ".kml";
+			eleFolder.addElement("Link").addElement("href").setText(relSubDocPath);
+			calTmp.set(Calendar.YEAR, calTmp.get(Calendar.YEAR) + 1);
+			calTmp.set(Calendar.MONTH, Calendar.JANUARY);
+			calTmp.set(Calendar.DAY_OF_MONTH, 1);
+		}		
+		try {
+			FileWriter fw = new FileWriter(kmlPath);
+			OutputFormat format = OutputFormat.createPrettyPrint();
+			XMLWriter writer = new XMLWriter(fw, format);
+			writer.write(kmlDoc);
+			writer.close();
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+		
+		// create sub kml documents
+		
+		
+		DailyRdahmmResultAnalyzer analyzer = analyzerTable.get(resUrl);
+		if (analyzer == null) {
+			analyzer = addAnalyzer(resUrl);
+		}
+		
+		DailyRdahmmStation[] stations = analyzer.stationArray;
+		
+		Element eleDoc = eleFolder.addElement("Document");
+		eleDoc.addElement("name").setText("Style Definitions");
+		String[] styleIds = {"greenIconStyle", "redIconStyle", "yellowIconStyle", "blueIconStyle", "grayIconStyle"};
+		String[] colorStrs = {"ff00ff00", "ff0000ff", "ff00ffff", "ffff0000", "ff7f7f7f"};
+		String[] urls = {"http://labs.google.com/ridefinder/images/mm_20_green.png",
+						"http://labs.google.com/ridefinder/images/mm_20_red.png",
+						"http://labs.google.com/ridefinder/images/mm_20_yellow.png",
+						"http://labs.google.com/ridefinder/images/mm_20_blue.png",
+						"http://labs.google.com/ridefinder/images/mm_20_gray.png"};
+		for (int i=0; i<5; i++) {
+			Element eleStyle = eleDoc.addElement("Style");
+			eleStyle.addAttribute("id", styleIds[i]);
+			Element eleIconStyle = eleStyle.addElement("IconStyle");
+			Element eleScale = eleIconStyle.addElement("scale");
+			eleScale.setText("0.7");
+			Element eleColor = eleIconStyle.addElement("color");
+			eleColor.setText(colorStrs[i]);
+			Element eleIcon = eleIconStyle.addElement("Icon");
+			Element eleHref = eleIcon.addElement("href");
+			eleHref.setText(urls[i]);
+			eleStyle.addElement("LabelStyle").addElement("scale").setText("0");
+		}
+		
+		while (calFrom.compareTo(calTo) <= 0) {
+			eleDoc = eleFolder.addElement("Document");
+			String dateStr = UtilSet.getDateString(calFrom);
+			eleDoc.addElement("name").setText(contextGroup + "_" + preTreat + "_" + dateStr);
+			// add the "TimeSpan" element
+			Element eleTP = eleDoc.addElement("TimeSpan");
+			eleTP.addElement("begin").setText(dateStr + "T00:00:00Z");
+			eleTP.addElement("end").setText(dateStr + "T23:59:59Z");
+			String colors = analyzer.calcStationColors(dateStr);
+			for (int i = 0; i < stations.length; i++) {
+				Element eleMark = eleDoc.addElement("Placemark");
+				Element eleName = eleMark.addElement("name");
+				eleName.setText(stations[i].stationID);
+				Element elePoint = eleMark.addElement("Point");
+				Element eleCoord = elePoint.addElement("coordinates");
+				eleCoord.setText(stations[i].longitude + "," + stations[i].latitude);
+				Element eleStyleUrl = eleMark.addElement("styleUrl");
+				int colorNum = Integer.valueOf(colors.substring(2*i, 2*i+1));
+				eleStyleUrl.setText("#" + styleIds[colorNum]);
+				if (calFrom.equals(calTo)) {
+					String fileUrlPrefix = analyzer.urlPattern.replace("{!station-id!}", stations[i].stationID) + "/" 
+											+ analyzer.dirPattern.replace("{!station-id!}", stations[i].stationID) + "/";
+					String rawUrl = fileUrlPrefix + analyzer.rawInputPattern.replace("{!station-id!}", stations[i].stationID);
+					String qUrl = fileUrlPrefix	+ analyzer.qPattern.replace("{!station-id!}", stations[i].stationID);
+					String popUpHtml = popUpWinHtml;
+					popUpHtml = popUpHtml.replace("{!rawFileURL!}", rawUrl);
+					popUpHtml = popUpHtml.replace("{!qFileURL!}", qUrl);
+					Element eleDesc = eleMark.addElement("description");
+					// eleDesc.setText("<![CDATA[" + popUpHtml + "]]");
+					eleDesc.setText(popUpHtml);
+				}
+			}
+			
+			calFrom.set(Calendar.DATE, calFrom.get(Calendar.DATE) + 1);
+		}
+		
+		try {
+			FileWriter fw = new FileWriter(kmlPath);
+			OutputFormat format = OutputFormat.createPrettyPrint();
+			XMLWriter writer = new XMLWriter(fw, format);
+			writer.write(kmlDoc);
+			writer.close();
+			fw.close();
+			
+			String res = UtilSet.exec("zip -D " + kmzFilePath + " " + kmlPath, new File(destKmlDir));
 			res = res.toLowerCase();
 			if (res.indexOf("error") >= 0 || res.indexOf("not found") >= 0) {
 				return kmlUrlPattern.replace("<fileName>", kmlFileName);
