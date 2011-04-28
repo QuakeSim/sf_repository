3077d00f13b846941b73bae4cd8289a15b444a3a
Marie <yuma@iu.edu>
2011-04-28 13:24:58 +0000

    Updated unavcotrans.py to parse pos files instead of csv files for UNAVCO stations, and generate LLH data directly

--- ExecutionServices/DailyRDAHMMService/src/main/webapp/WEB-INF/binaryexec/unavcotrans.py
+++ ExecutionServices/DailyRDAHMMService/src/main/webapp/WEB-INF/binaryexec/unavcotrans.py
@@ -10,7 +10,7 @@
 #   python unavcotrans.py stationID destdir beginDate endDate
 #
 # output:
-#   stationID.pbo.igs05.csv [daily_project_stationID].unavco.raw
+#   stationID.pbo.igs05.pos [daily_project_stationID].unavco.raw
 #============================================
 
 import csv, sys, os, subprocess, urllib
@@ -18,7 +18,6 @@ from datetime import datetime
 
 numargv = len(sys.argv)
 unavco_client_path = "./unavcoClients"
-cs2cs_exec = "./cs2cs"
 
 if numargv == 1:
     sys.exit("usage: unavcotrans.py stationID destdir beginDate endDate")
@@ -36,41 +35,35 @@ elif numargv == 5:
     # os.chdir(destdir)
 
     # query UNAVCO for station data file in .pbo.igs05.csv format
-    #cmd = "java -jar " + unavco_client_path + "/unavcoFiles.jar -4charEqu " + stationID.upper() + " -dataStartDate " + bdate + " -dataEndDate " + edate + " -positionCSV"
-    cmd = "java -jar " + unavco_client_path + "/unavcoFiles.jar -4charEqu " + stationID.upper() + " -positionCSV"
+    cmd = "java -jar " + unavco_client_path + "/unavcoFiles.jar -4charEqu " + stationID.upper() + " -position"
     filelist = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE).communicate()[0].split("\n")
     fileloc = None
     for item in filelist:
-        ind = item.find(".pbo.igs05.csv")
+        ind = item.find(".pbo.igs05.pos")
         if not ind == -1:
-            fileloc = item[:ind] + ".pbo.igs05.csv"
+            fileloc = item[:ind] + ".pbo.igs05.pos"
     if fileloc == None:
         sys.exit("No data available for station " + stationID)
 
     # download the original UNAVCO data file
-    unavcofile = destdir + "/" + stationID + ".pbo.igs05.csv"
+    unavcofile = destdir + "/" + stationID + ".pbo.igs05.pos"
     urllib.urlretrieve(fileloc, unavcofile)
 
     # convert UNAVCO data file into GRWS raw format
     rawfile = destdir + "/daily_project_" + stationID + ".unavco.raw"
-    inputReader = csv.reader(open(unavcofile, 'r'), delimiter = ',')
+    inputReader = csv.reader(open(unavcofile, 'r'), delimiter = ' ')
     outputWriter = csv.writer(open(rawfile, 'w'), delimiter = ' ')
     for row in inputReader:
-        if row[0] == "Reference position":
-            lat = row[1].split()[0]
-            long = row[2].split()[0]
-            vert = row[3].split()[0]
-            cmd = cs2cs_exec + " -f '%.4f' +proj=latlong +datum=WGS84 +to +proj=geocent +datum=WGS84 <<EOF \n" + long + " " + lat + " " + vert
-            x, y, z = map(float, subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE).communicate()[0].split())
-        if len(row) >= 8 and row[0][:4].isdigit():
-            rdate = datetime.strptime(row[0], "%Y-%m-%d")
+        row = filter(None, row)
+        if len(row) >= 25: 
+            rdate = datetime.strptime(row[0]+" "+row[1], "%Y%m%d %H%M%S")
             if rdate >= bdate and rdate <= edate: 
-                date = row[0] + "T12:00:00"
-                xx, yy, zz, dx, dy, dz = [val/1000.0 for val in map(float, row[1:7])]
-                ux = x + xx
-                uy = y + yy
-                uz = z + zz
-                outputWriter.writerow([stationID, date, ux, uy, uz, dx, dy, dz])
+                date = rdate.strftime("%Y-%m-%dT%H:%M:%S")
+                lat, lon, vert = map(float, row[12:15])
+		if lon > 180:
+		    lon = lon - 360
+                dlat, dlon, dvert = map(float, row[18:21])
+                outputWriter.writerow([stationID, date, lon, lat, vert, dlon, dlat, dvert])
     del inputReader, outputWriter
     print rawfile
 else:
