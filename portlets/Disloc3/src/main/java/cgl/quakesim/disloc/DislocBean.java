83b0a25ff9714c0a64583b0e4d343eeaec137ff9
Marlon Pierce <marpierc@iu.edu>
2011-05-10 15:08:22 +0000

    Updating disloc portlets to look like the anonymous project.

--- portlets/Disloc3/src/main/java/cgl/quakesim/disloc/DislocBean.java
+++ portlets/Disloc3/src/main/java/cgl/quakesim/disloc/DislocBean.java
@@ -1186,7 +1186,8 @@ public class DislocBean extends GenericSopacBean implements HttpSessionBindingLi
 
 	 /**
 	  * These are two trivial wrappers around toggleProjectSelection() that are 
-	  * useful for DashboardPanelAnon.
+	  * useful for DashboardPanelAnon.  We need these because we separate the 
+	  * selection ation into two separate forms.
 	  */
 	 public void toggleProjectSelectionAnon1(ActionEvent ev) {
 		  toggleProjectSelection(ev);
