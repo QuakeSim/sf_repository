1a0a1bec6c8f2c6d3d3a76bf6588ba7b3ab76263
Marlon Pierce <marpierc@iu.edu>
2011-10-31 14:08:14 +0000

    Hopefully fixing arrow scaling

--- ExecutionServices/SimpleXService/src/main/java/cgl/webservices/simplex/SimpleXService.java
+++ ExecutionServices/SimpleXService/src/main/java/cgl/webservices/simplex/SimpleXService.java
@@ -1369,12 +1369,19 @@ public class SimpleXService extends AntVisco implements Runnable {
 			 
 			 //Determine the scaling. We'll need to use one value for all 
 			 //arrow plots, so we call the method multiple times.
-			 double arrowScale=setGlobalKmlArrowScale(residualPointEntries);
-			 logger.debug("ArrowScale:"+arrowScale);
-			 arrowScale=setGlobalKmlArrowScale(calcPointEntries);			
-			 logger.debug("ArrowScale:"+arrowScale);
-			 arrowScale=setGlobalKmlArrowScale(obsvPointEntries);
-			 logger.debug("ArrowScale:"+arrowScale);
+			 double[] arrowScale=new double[3];
+			 arrowScale[0]=setGlobalKmlArrowScale(residualPointEntries);
+			 logger.debug("ArrowScale:"+arrowScale[0]);
+			 arrowScale[1]=setGlobalKmlArrowScale(calcPointEntries);			
+			 logger.debug("ArrowScale:"+arrowScale[1]);
+			 arrowScale[2]=setGlobalKmlArrowScale(obsvPointEntries);
+			 logger.debug("ArrowScale:"+arrowScale[2]);
+			 
+			 //Sort the arrowScale values from lowest to highest.  It is 
+			 //also possible in the future that we need to retain the 3 different
+			 //scales.  For now, though, we use the longest arrowScale result
+			 //for all.
+			 Arrays.sort(arrowScale);
 
 			//Set up the session wide service coordinates.
 			SimpleXDataKml kmlService;
@@ -1392,20 +1399,20 @@ public class SimpleXService extends AntVisco implements Runnable {
 			kmlService.setArrowPlacemark("'Residual Displacements Arrow Layer", 
 												  "ffff0000", 
 												  2, 
-												  arrowScale);
+												  arrowScale[arrowScale.length-1]);
 
 			kmlService.setDatalist(calcPointEntries);
 			kmlService.setArrowPlacemark("Calculated Displacements Arrow Layer", 
 												  "ff00ccff", 
 												  2, 
-												  arrowScale);
+												  arrowScale[arrowScale.length-1]);
 
 
 			kmlService.setDatalist(obsvPointEntries);
 			kmlService.setArrowPlacemark("Observed Displacements Arrow Layer", 
 												  "ff0000ff", 
 												  2,
-												  arrowScale);
+												  arrowScale[arrowScale.length-1]);
 			String totalKmlUrl = kmlService.runMakeKml("", 
 																	 userName,
 																	 projectName,
