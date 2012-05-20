ab88de6c1b969a8c40e16b62b3cb842c57941130
Marie <yuma@iu.edu>
2012-05-20 21:55:41 +0000

    updated README

--- PythonRDAHMM/check_missing_data.py
+++ PythonRDAHMM/check_missing_data.py
@@ -15,9 +15,10 @@ for dataset in os.listdir(data_path):
     src = data_path + "/" + dataset + "/"
     for dbfile in glob.glob(src +"/????.sqlite"):
         stationID = string.split(dbfile, "/")[-1][:-7]
-        model_dest = model_path + "/" + dataset + "/" + "daily_project_" + stationID
+        model_dest = model_path + "/" + dataset + "/" + "daily_project_" + stationID + "/*.Q"
         eval_dest = eval_path + "/" + dataset + "/" + "daily_project_" + stationID + "*"
-        if not os.path.exists(model_dest):
-            print model_dest
+        #if not os.path.exists(model_dest):
+        #if glob.glob(model_dest) == []:
+        #    print model_dest
         if glob.glob(eval_dest) == []:
             print eval_dest
